cf6b562859fc4f59a57c8d4425e617d578613eb5
Anibal Ventura <anibalventura123@gmail.com>
2021-10-30 18:59:58 -0500

    Add linearguments

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 from Lexer.lexer import LexicalAnalyzer
 from Parser.parse import parse_words
 from symbols import grammar_text, token_rules
+import sys
 
 if __name__ == '__main__':
+    if(len(sys.argv) < 2 ):
+        raise Exception("Need input filename as argument")
+    filename = sys.argv[1]
+    
     analyzer = LexicalAnalyzer()
 
     token = []
@@ -10,7 +15,8 @@ if __name__ == '__main__':
     row = []
     column = []
 
-    f= open('program_example.aud','r')
+
+    f= open(filename,'r')
     contents = f.read()
     tokens, lexemes, lines, columns = analyzer.tokenize(token_rules, contents)
     
